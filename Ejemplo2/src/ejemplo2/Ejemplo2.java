f1166f59baa544e2d30e2c061c56c74f7ed95b67
CV <CV@DESKTOP-7432IHD>
2017-09-28 13:41:44 -0600

    Version 1.0

---
+++ Ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,14 @@
+
+package ejemplo2;
+
+
+public class Ejemplo2 {
+
+    public static void main(String[] args) {
+      for(int i = 0; i<2;i++){
+          System.out.println("Hola Mundo");
+      }
+        System.out.println("Finalizo");
+    }
+    
+}
